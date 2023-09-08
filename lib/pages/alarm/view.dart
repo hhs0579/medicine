1e452233e3cb7b552d2e7e2a736362c8db3a8c4e
HiImYong99 <good19422@naver.com>
2023-09-09 01:24:33 +0900

    23.09.09 요일 이름 설정
    but 드롭다운에서 value 선택하면 분명 value값은 전달 되는데 보이는 값은 계속 일요일임

--- lib/pages/alarm/view.dart
+++ lib/pages/alarm/view.dart
@@ -2,7 +2,45 @@ import 'package:flutter/material.dart';
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'package:timezone/data/latest.dart' as tz;
 import 'package:timezone/timezone.dart' as tz;
-import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+
+void main() async {
+  tz.initializeTimeZones();
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
+      FlutterLocalNotificationsPlugin();
+  late AndroidNotificationChannel channel;
+
+  MyApp() {
+    final settingsAndroid = AndroidInitializationSettings('app_icon');
+    final initializationSettings =
+        InitializationSettings(android: settingsAndroid);
+
+    flutterLocalNotificationsPlugin.initialize(initializationSettings);
+
+    channel = AndroidNotificationChannel(
+      'channel_id',
+      'Channel Name',
+      importance: Importance.high,
+      playSound: true,
+    );
+
+    flutterLocalNotificationsPlugin
+        .resolvePlatformSpecificImplementation<
+            AndroidFlutterLocalNotificationsPlugin>()
+        ?.createNotificationChannel(channel);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: MyAlarm(),
+    );
+  }
+}
 
 class MyAlarm extends StatefulWidget {
   const MyAlarm({Key? key}) : super(key: key);
@@ -42,6 +80,9 @@ class _MyAlarmState extends State<MyAlarm> {
       scheduledTime = scheduledTime.add(Duration(days: 1));
     }
 
+    int daysToAdd = (alarm.dayOfWeek - now.weekday + 7) % 7;
+    scheduledTime = scheduledTime.add(Duration(days: daysToAdd));
+
     try {
       var androidPlatformChannelSpecifics = AndroidNotificationDetails(
         'channel_id',
@@ -57,13 +98,13 @@ class _MyAlarmState extends State<MyAlarm> {
       await flutterLocalNotificationsPlugin.zonedSchedule(
         alarm.hashCode, // Use a unique ID for each alarm
         '알림',
-        '알림이 설정된 시간입니다.',
+        '${alarm.name} 알림이 설정된 시간입니다.',
         tz.TZDateTime.from(scheduledTime, tz.local),
-      platformChannelSpecifics,
-      uiLocalNotificationDateInterpretation:
-          UILocalNotificationDateInterpretation.absoluteTime,
-      androidAllowWhileIdle: true,
-    );
+        platformChannelSpecifics,
+        uiLocalNotificationDateInterpretation:
+            UILocalNotificationDateInterpretation.absoluteTime,
+        androidAllowWhileIdle: true,
+      );
 
       setState(() {
         alarms.add(alarm);
@@ -80,9 +121,96 @@ class _MyAlarmState extends State<MyAlarm> {
     );
 
     if (pickedTime != null) {
-      final alarm = Alarm(time: pickedTime);
-      _scheduleNotification(alarm);
+      // 변경: 초기값을 null 대신 0으로 설정
+      final alarm = Alarm(time: pickedTime, dayOfWeek: 0, name: ''); // 초기값 설정
+      _showAlarmDialog(context, alarm);
     }
+  }
+
+  Future<void> _showAlarmDialog(BuildContext context, Alarm alarm) async {
+    TimeOfDay pickedTime = alarm.time;
+    int selectedDay = alarm.dayOfWeek;
+    String alarmName = alarm.name;
+
+    await showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text('알림 설정'),
+          content: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: <Widget>[
+              DropdownButton<int>(
+                value: selectedDay,
+                items: [
+                  DropdownMenuItem<int>(
+                    value: 0,
+                    child: Text('일요일'),
+                  ),
+                  DropdownMenuItem<int>(
+                    value: 1,
+                    child: Text('월요일'),
+                  ),
+                  DropdownMenuItem<int>(
+                    value: 2,
+                    child: Text('화요일'),
+                  ),
+                  DropdownMenuItem<int>(
+                    value: 3,
+                    child: Text('수요일'),
+                  ),
+                  DropdownMenuItem<int>(
+                    value: 4,
+                    child: Text('목요일'),
+                  ),
+                  DropdownMenuItem<int>(
+                    value: 5,
+                    child: Text('금요일'),
+                  ),
+                  DropdownMenuItem<int>(
+                    value: 6,
+                    child: Text('토요일'),
+                  ),
+                ],
+                onChanged: (value) {
+                  setState(() {
+                    selectedDay = value!;
+                  });
+                },
+              ),
+              TextField(
+                decoration: InputDecoration(labelText: '알림 이름'),
+                onChanged: (value) {
+                  setState(() {
+                    alarmName = value;
+                  });
+                },
+              ),
+            ],
+          ),
+          actions: <Widget>[
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+              child: Text('취소'),
+            ),
+            TextButton(
+              onPressed: () {
+                final updatedAlarm = Alarm(
+                  time: pickedTime,
+                  dayOfWeek: selectedDay,
+                  name: alarmName,
+                );
+                _scheduleNotification(updatedAlarm);
+                Navigator.of(context).pop();
+              },
+              child: Text('저장'),
+            ),
+          ],
+        );
+      },
+    );
   }
 
   Widget _buildAlarmList() {
@@ -102,7 +230,9 @@ class _MyAlarmState extends State<MyAlarm> {
         else
           ...alarms.map(
             (alarm) => ListTile(
-              title: Text('알림 시간: ${alarm.time.format(context)}'),
+              title: Text('요일: ${_getDayOfWeekText(alarm.dayOfWeek)}'), // 요일 표시
+              subtitle: Text(
+                  '알림 시간: ${alarm.time.format(context)}\n알림 이름: ${alarm.name}'), // 알림 시간과 이름 표시
               trailing: IconButton(
                 icon: Icon(Icons.delete),
                 onPressed: () {
@@ -113,6 +243,27 @@ class _MyAlarmState extends State<MyAlarm> {
           ),
       ],
     );
+  }
+
+  String _getDayOfWeekText(int dayOfWeek) {
+    switch (dayOfWeek) {
+      case 0:
+        return '일요일';
+      case 1:
+        return '월요일';
+      case 2:
+        return '화요일';
+      case 3:
+        return '수요일';
+      case 4:
+        return '목요일';
+      case 5:
+        return '금요일';
+      case 6:
+        return '토요일';
+      default:
+        return '';
+    }
   }
 
   Future<void> _cancelNotification(int id) async {
@@ -156,6 +307,8 @@ class _MyAlarmState extends State<MyAlarm> {
 
 class Alarm {
   final TimeOfDay time;
+  final int dayOfWeek;
+  final String name;
 
-  Alarm({required this.time});
+  Alarm({required this.time, required this.dayOfWeek, required this.name});
 }
